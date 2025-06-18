9212c85e21e7b3c28905b853288eb94fc14d7513
avatarfeed <abxyaxfb@gmail.com>
2025-06-18 20:17:25 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -71,4 +71,4 @@ def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=10000)
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
